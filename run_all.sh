793c53cae577b0464282d460496548cb2c020113
Zhaohq96 <126513822+Zhaohq96@users.noreply.github.com>
2025-02-01 20:14:17 +0100

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -4,7 +4,7 @@
 #datasets=("mild-bottleneck-1K" "severe-bottleneck-1K" "recent-migration-1K" "old-migration-1K" "low-intensity-recombination-hotspot-1K" "high-intensity-recombination-hotspot-1K") 
 #datasets=("mild-bottleneck-5K" "severe-bottleneck-5K" "recent-migration-5K" "old-migration-5K" "low-intensity-recombination-hotspot-5K" "high-intensity-recombination-hotspot-5K")
 datasets=("mild-bottleneck" "severe-bottleneck" "recent-migration" "old-migration" "low-intensity-recombination-hotspot" "high-intensity-recombination-hotspot") # "severe-bottleneck-5K") # "low-intensity-recombination-hotspot-1K" "low-intensity-recombination-hotspot-5K") 
-size=1K # size of the dataset for training the models
+size=5K # size of the dataset for training the models
 grid=100 # grid size
 
 for dataset in "${datasets[@]}"
